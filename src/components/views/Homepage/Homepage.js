dbb1851bfde61d544a6b4184e26ef39bad42954f
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-11-02 16:49:51 +0100

    Add autoRotate and adjust autoRotate speed

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -21,7 +21,7 @@ const Component = ({className}) => (
     >
       <ambientLight intensity={0.5} />
       <spotLight position={[15, 20, 5]} penumbra={1} castShadow />
-      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+      <OrbitControls autoRotate autoRotateSpeed={0.3} enablePan={true} enableZoom={true} enableRotate={true} />
       <Box args={[1, 1, 1]} position={[0, 1, -5]}/>
       <Box args={[1, 1, 1]} position={[0, 1, 2]}/>
       <Box args={[1, 1, 1]} position={[-4, 1, 0]}/>
@@ -30,7 +30,7 @@ const Component = ({className}) => (
         radius={100} // Radius of the inner sphere (default=100)
         depth={50} // Depth of area where stars should fit (default=50)
         count={5000} // Amount of stars (default=5000)
-        factor={4} // Size factor (default=4)
+        factor={5} // Size factor (default=4)
         saturation={0} // Saturation 0-1 (default=0)
         fade // Faded dots (default=false)
       />
