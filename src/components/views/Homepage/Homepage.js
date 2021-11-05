eaca91bbce59653c00d7a4a32af64c8aaaf711c1
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-11-05 15:05:08 +0100

    Adjust stars

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -59,11 +59,11 @@ const Component = ({className, getProjects, projects, loading, error}) => {
             ))}
           </Suspense>
           <Stars
-            radius={100} // Radius of the inner sphere (default=100)
-            depth={50} // Depth of area where stars should fit (default=50)
-            count={6000} // Amount of stars (default=5000)
-            factor={5} // Size factor (default=4)
-            saturation={0} // Saturation 0-1 (default=0)
+            radius={400} // Radius of the inner sphere (default=100)
+            depth={100} // Depth of area where stars should fit (default=50)
+            count={5000} // Amount of stars (default=5000)
+            factor={15} // Size factor (default=4)
+            saturation={0.2} // Saturation 0-1 (default=0)
             fade // Faded dots (default=false)
           />
           {/*
