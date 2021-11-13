5478675a5e96bae50a7df7bf607677fc2c28e915
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-11-13 10:38:22 +0100

    Adjust button

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -7,7 +7,7 @@ import styles from './Button.module.scss';
 export const Button = ({className, src, title}) => <a className={clsx(className, styles.root, styles.btn)} href={src}>{title}</a>;
 
 
-Component.propTypes = {
+Button.propTypes = {
   className: PropTypes.string,
   src: PropTypes.string,
   title: PropTypes.string,
